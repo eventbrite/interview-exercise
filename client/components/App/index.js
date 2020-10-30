0d61429ec240a22ef8ede4eda7b0dbb448ffb1ac
Kody Rouse <59030518+slingshot-kodyrouse@users.noreply.github.com>
2020-10-30 17:16:33 -0400

    restructured project with separated folders

---
+++ client/components/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => (
+  <section className="eds-l-mar-all-4">
+    <h1>This is your App ! Yay</h1>
+    <p>{serverMessage}</p>
+  </section>
+);
+
+export default App;
+
+// const [serverMessage, setServerMessage] = useState('Calling API...');
+
+//   useEffect(() => {
+//     fetch('./api').then(
+//       response => response.json()
+//     ).then(
+//       payload => setServerMessage(payload.message)
+//     );
+//   }, []);
